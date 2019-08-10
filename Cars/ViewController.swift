3591383aa30490b2022fec1f50cf79a454156f21
Artem Podustov <artem.podustov@xyrality.com>
2019-08-10 06:26:53 +0200

    Initial Commit

---
+++ Cars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cars
+//
+//  Created by Artem Podustov on 8/10/19.
+//  Copyright © 2019 podustov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
